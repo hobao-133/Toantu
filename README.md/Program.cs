8d61355ddf3aeee370f15f132634664817852cc0
hobao-133 <kevinbh133@gmail.com>
2024-08-09 17:50:16 +0700

    done

---
+++ README.md/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Toantu;
+class Program{
+    static void Main(string [] args){
+        float width;
+        float height;
+        Console.Write("Enter width: ");
+        width = float.Parse(Console.ReadLine());
+        Console.Write("Enter height: ");
+        height = float.Parse(Console.ReadLine());
+        float area = width * height;
+        Console.WriteLine("Area is: " + area);
+    }
+}
